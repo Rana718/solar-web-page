057a39d46856e6f125531b7995402545bd2ca93d
Rana718 <ranadolui718@gmail.com>
2024-12-07 00:27:25 +0530

    add the video

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,10 @@
 import solar from './image/solar.jpg';
-
+import solar1 from './video/solar1.mp4';
+import solar2 from './video/solar2.mp4';
 
 
 export {
-    solar
+    solar,
+    solar1,
+    solar2
 }
