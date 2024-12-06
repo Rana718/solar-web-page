8ab107e08cff487634efb80876cfce14c7639819
Rana718 <ranadolui718@gmail.com>
2024-12-07 00:28:41 +0530

    fix the SectionWrapper code

--- src/hooks/SectionWrapper.jsx
+++ src/hooks/SectionWrapper.jsx
@@ -1,8 +1,6 @@
 import { motion } from "framer-motion";
 import React from "react";
 
-
-
 const staggerContainer = (staggerChildren, delayChildren = 0) => {
     return {
         hidden: {},
@@ -18,19 +16,19 @@ const staggerContainer = (staggerChildren, delayChildren = 0) => {
 const StarWrapper = (Component, idName) =>
     function HOC() {
         return (
-            <motion.section
-                variants={staggerContainer(0.5, 0.25)}
-                initial="hidden"
-                whileInView="show"
-                viewport={{ once: true, amount: 0.25 }}
-                className={`sm:px-16 px-6 sm:py-16 py-10 max-w-7xl mx-auto relative z-0`}
+            <section
+                id={idName}
+                className="relative z-0 w-full"
             >
-                <span className="hash-span" id={idName}>
-                    &nbsp;
-                </span>
-
-                <Component />
-            </motion.section>
+                <motion.div
+                    variants={staggerContainer(0.5, 0.25)}
+                    initial="hidden"
+                    whileInView="show"
+                    viewport={{ once: true, amount: 0.25 }}
+                >
+                    <Component />
+                </motion.div>
+            </section>
         );
     };
 
